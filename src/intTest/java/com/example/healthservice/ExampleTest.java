709cfabcfe3d8b9d3cf4115fddc70aa7d1e08f82
Jan Mewes <jan.mewes@ks-plus.org>
2023-07-19 10:45:12 +0200

    chore: add inttest source set

---
+++ src/intTest/java/com/example/healthservice/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.example.healthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExampleTest {
+
+    @Test
+    void should_load_context() {
+
+    }
+}
---
+++ src/intTest/java/com/example/healthservice/HealthServiceApplication.java
@@ -0,0 +1,7 @@
+package com.example.healthservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceApplication {
+}
